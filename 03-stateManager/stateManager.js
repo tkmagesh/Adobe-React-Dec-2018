71147b4f83a85a0b789157c9cb338ed97bf74c41
Magesh <tkmagesh77@gmail.com>
2018-12-04 16:39:00 +0530

    state manager implementation

---
+++ 03-stateManager/stateManager.js
@@ -0,0 +1,36 @@
+var SM = (function(){	
+	var _currentState = undefined,
+		_subscribers = [],
+		_reducer = null,
+		__init_action = '@@INIT_ACTION'
+
+
+	function getState(){
+		return _currentState;
+	}
+
+	function subscribe(callback){
+		if (typeof callback === 'function')
+			_subscribers.push(callback);
+	}
+
+	function triggerChange(){
+		_subscribers.forEach(callback => callback());
+	}
+
+	function dispatch(action){
+		var newState = _reducer(_currentState, action);
+		if (newState === _currentState) return;
+		_currentState = newState;
+		triggerChange();
+	}
+
+	function createStore(reducer){
+		_reducer = reducer;
+		_currentState = _reducer(_currentState, __init_action);
+		return { getState, subscribe, dispatch };
+	}
+
+	return { createStore };
+
+})();
